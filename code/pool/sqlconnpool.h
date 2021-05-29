4469ee1e67bd21bf7297d83ba8a340d02a593abf
shenglong_zhou <shenglongzhou97@gmail.com>
2021-05-29 18:48:52 +0800

    added sqlconnpool

---
+++ code/pool/sqlconnpool.h
@@ -0,0 +1,42 @@
+//
+// Created by azhou on 2021/5/29.
+//
+
+#ifndef WEBSERVER_SQLCONNPOOL_H
+#define WEBSERVER_SQLCONNPOOL_H
+
+#include <mysql/mysql.h>
+#include <string>
+#include <queue>
+#include <mutex>
+#include <semaphore.h>
+#include <thread>
+#include "../log/log.h"
+
+class SqlConnPool{
+public:
+    static SqlConnPool* Instance();
+
+    MYSQL* GetConn();
+    void FreeConn(MYSQL* conn);
+    int GetFreeConnCount();
+
+    void Init(const char* host, int port,
+              const char* user, const char* pwd,
+              const char* dbName, int connSize);
+    void ClosePool();
+
+private:
+    SqlConnPool();
+    ~SqlConnPool();
+
+    int MAX_CONN_;
+    int useCount_;
+    int freeCount_;
+
+    std::queue<MYSQL* > connQue_;
+    std::mutex mtx_;
+    sem_t  semId;
+};
+
+#endif //WEBSERVER_SQLCONNPOOL_H
