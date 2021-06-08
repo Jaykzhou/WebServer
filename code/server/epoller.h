a743f9895e6dbfbea501d65bcf9df1f1cb84cecc
shenglong_zhou <shenglongzhou97@gmail.com>
2021-06-08 15:10:39 +0800

    epoller

---
+++ code/server/epoller.h
@@ -0,0 +1,34 @@
+//
+// Created by azhou on 2021/6/8.
+//
+
+#ifndef WEBSERVER_EPOLLER_H
+#define WEBSERVER_EPOLLER_H
+
+#include <sys/epoll.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <vector>
+#include <assert.h>
+#include <errno.h>
+
+class Epoller {
+public:
+    explicit Epoller(int maxEvent = 1024);
+
+    ~Epoller();
+
+    bool AddFd(int fd, uint32_t events);
+    bool ModFd(int fd, uint32_t events);
+    bool DelFd(int fd);
+
+    int Wait(int timeoutMs = -1);
+    int GetEventFd(size_t i) const;
+    uint32_t GetEvents(size_t i) const;
+
+private:
+    int epollFd_;
+
+    std::vector<struct epoll_event> events_;
+};
+#endif //WEBSERVER_EPOLLER_H
